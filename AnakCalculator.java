ae9bf5f63d425ad8e2d8354e59ecff4dff93db6c
okiarif <32841660+okiarif@users.noreply.github.com>
2017-11-06 21:06:42 +0700

    Add files via upload

---
+++ AnakCalculator.java
@@ -0,0 +1,10 @@
+public class AnakCalculator {
+	
+	public static void main(String args[]){
+		Calculator cal = new
+		Calculator();
+		cal.pertambahan(a, b);
+		cal.pengurangan(a, b);
+		
+}
+}
---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+	int z;
+	protected void pertambahan(int x, int y )
+{	z= x + y ;
+	System.out.println("Hasil:" + z);
+}
+	protected void pengurangan(int x, int y )
+{	z= x - y ;
+	System.out.println("Hasil:" + z);
+	}
+	
+	}
